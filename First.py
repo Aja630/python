df27e2064e04dc712dc02b7a2a8bd71a04c131e1
Aja630 <ajayahirwar1999.srj@gmail.com>
2025-05-06 15:56:41 +0530

    first commit

---
+++ First.py
@@ -0,0 +1,150 @@
+'''x=10
+y=20
+
+print(x<y)
+print(x>y)
+print(x==y)
+print(x!=y)
+print(x<=y)
+print(x>=y)
+
+iv.logical operators { and,or,not }
+'''
+'''x=10
+y=20
+z=30
+print(x>y and x>z)
+print(z>x and z>y)
+
+
+
+v. membership operator {in,not,in} compair on the basis of address
+'''
+'''x='Ajy'
+print('a' in x)
+
+print('A' in x)
+print('a' not in x)
+print('A' not in x)
+
+
+
+vi. Identity {is,is not}
+'''
+
+# x = 10
+# print(id(x))
+
+# y = 10
+# print(id(y))
+
+# print(x is y)
+
+# x = 10
+# print(id(x))
+
+# y = 20
+# print(id(y))
+
+# print(x is y)
+
+# Bitwise operator:-{&,~,<<,>>,!}
+
+# x=10
+# y=20
+# print(x & y)
+
+# x=3
+# y=7
+# print(3 | 7 ) 
+
+
+# left-shift(<<):-    16 8 4 2 1
+#                      0 1 0 1 0
+#                    0 1 0 1 0 0  =20ans---interview related
+
+# x=10
+# print(x << 1)
+
+# Right-shift(>>)
+            
+#     x=10
+#     print(x >> 1)      ----10/2=5 ans
+    
+
+# x=int()
+# print(x)           => integer value
+# print (type(x))
+
+
+# x=float()
+# print (x)          =>float value
+# print (type(x))
+
+
+# x=complex()
+
+
+# x=0
+# print(x)
+# print(type(x))
+
+# x=0.0
+# print(x)
+# print(type(x))
+
+# x=[]
+# print(x)
+# print(type(x))
+
+
+# x=input("enter any no")
+# print(x)
+# print(type(x))
+
+
+# x=int(input("enter any no"))
+# print(x)
+# print(type(x))
+
+# x=eval(input("enter anything"))
+# print(x)
+# print(type(x))
+
+
+# in-built function    in-built class
+# print()               int
+# type()                str
+# id()                  list
+# input()               tupk
+# eval()                dick
+# max()                 set
+# min()                  
+# sum()
+# len()
+
+# s='Python'
+# print(max(s))
+
+# print(min(s))
+# print(len(s))
+# print(sum(s))
+
+
+# l=[10, 20, 30, 'p']
+# print(max(l))
+# print(min(l))
+# print(len(l))
+# print(sum(l))
+
+print("Hello")
+print("Hi")
+
+x=10
+y=20
+
+print( x,y )  or print(x,y, sep=',')
+
+
+print("Hello",end=',')
+print("Hi")
